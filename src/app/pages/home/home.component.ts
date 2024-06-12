540b33f4acf64aa56d86050ecf491bdaeef6e6f4
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2024-06-12 10:18:20 -0300

    feat: updated home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,42 +1,13 @@
-import { isPlatformBrowser } from '@angular/common';
-import {
-	Component,
-	ElementRef,
-	Inject,
-	PLATFORM_ID,
-	ViewChild,
-} from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { CharacterComponent } from '../../components/character/character.component';
-import { TextSliderComponent } from '../../components/text-slider/text-slider.component';
-import { preventAnimationWhenPageLoad } from '../../utils/preventAnimationWhenPageLoad';
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { HomeIntroductionComponent } from '../../components/sections/home/home-introduction/home-introduction.component';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [TextSliderComponent, CharacterComponent, RouterLink],
+	imports: [ButtonComponent, HeaderComponent, HomeIntroductionComponent],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-	img_star: string = '../../../assets/shapes/star.svg';
-	hamMenu = true;
-	hamMenuActive = true;
-
-	@ViewChild('header') headerRef!: ElementRef<HTMLElement>;
-
-	toggle() {
-		this.hamMenuActive = !this.hamMenuActive;
-	}
-
-	constructor(
-		@Inject(PLATFORM_ID)
-		private platformId: any
-	) {}
-
-	ngAfterViewInit() {
-		if (isPlatformBrowser(this.platformId)) {
-			preventAnimationWhenPageLoad(this.headerRef.nativeElement, 700);
-		}
-	}
-}
+export class HomeComponent {}
