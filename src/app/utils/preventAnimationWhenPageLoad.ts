ed6fca5a0028806d3b257a0aef222a4caf498b02
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2024-06-12 09:58:31 -0300

    feat: updated prevent animation utility

--- src/app/utils/preventAnimationWhenPageLoad.ts
+++ src/app/utils/preventAnimationWhenPageLoad.ts
@@ -2,6 +2,8 @@ export function preventAnimationWhenPageLoad(
 	element: HTMLElement,
 	duration: number = 500
 ) {
+	element.classList.add('prevent_animation_when_page_load');
+
 	setTimeout(() => {
 		element.classList.remove('prevent_animation_when_page_load');
 	}, duration);
