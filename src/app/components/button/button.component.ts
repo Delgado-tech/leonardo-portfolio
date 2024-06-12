740b8597edff523ce1bf04ffa86daf3b6864d421
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2024-06-12 10:02:54 -0300

    feat: added button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+type ButtonColorType = 'primary' | 'gray';
+type AnchorTargetType = '_self' | '_blank';
+
+@Component({
+	selector: 'app-button',
+	standalone: true,
+	imports: [RouterLink],
+	templateUrl: './button.component.html',
+	styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+	@ViewChild('buttonRef') buttonRef!: ElementRef<HTMLButtonElement>;
+
+	@Input('href') href: string = '#';
+	@Input('routerLink') public routerLink: string | undefined;
+	@Input('target') target: AnchorTargetType = '_self';
+	@Input('bordered') bordered: boolean = false;
+	@Input('color') color: ButtonColorType = 'primary';
+
+	ngAfterViewInit() {
+		if (this.bordered) this.buttonRef.nativeElement.classList.add('bordered');
+		this.buttonRef.nativeElement.classList.add(this.color);
+	}
+}
