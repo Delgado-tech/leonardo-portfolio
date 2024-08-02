67383fe7082d5a66c988580336401cd0f2358f63
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2024-08-01 21:12:57 -0300

    feat: added banner images in project-examples-banners

--- src/app/components/sections/home/project-examples-banners/project-examples-banners.component.ts
+++ src/app/components/sections/home/project-examples-banners/project-examples-banners.component.ts
@@ -14,22 +14,60 @@ interface IBannerSliderContent {
 	styleUrl: './project-examples-banners.component.scss',
 })
 export class ProjectExamplesBannersComponent {
+	bannersPath: string = '../../../../../assets/project-examples-banners';
+
 	bannerSliders: IBannerSliderContent[] = [
 		{
-			slides: ['', '', '', '', ''],
+			slides: [
+				'tenshoes.png',
+				'techtrends.png',
+				'powerfit.png',
+				'english_elevate.png',
+				'portfolio_maria.png',
+				'pettzjoin.png',
+			],
 			reverse: false,
 		},
 		{
-			slides: ['', '', '', '', ''],
+			slides: [
+				'jollies.png',
+				'sabrasa.png',
+				'portfolio_felipe.png',
+				'concimotors.png',
+				'beaute.png',
+				'maisons.png',
+			],
 			reverse: true,
 		},
 		{
-			slides: ['', '', '', '', ''],
+			slides: [
+				'pettzjoin.png',
+				'portfolio_maria.png',
+				'english_elevate.png',
+				'powerfit.png',
+				'techtrends.png',
+				'tenshoes.png',
+			],
 			reverse: false,
 		},
 		{
-			slides: ['', '', '', '', ''],
+			slides: [
+				'maisons.png',
+				'beaute.png',
+				'concimotors.png',
+				'portfolio_felipe.png',
+				'sabrasa.png',
+				'jollies.png',
+			],
 			reverse: true,
 		},
 	];
+
+	getSlideURL(slideName: string): string {
+		if (slideName === '') {
+			return slideName;
+		}
+
+		return `url('${this.bannersPath}/${slideName}');`;
+	}
 }
