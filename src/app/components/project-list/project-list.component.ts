0b60ba9a18e9a95b9da36102f6b71b4a96a8d85e
Delgado-tech <leonardo.delgadosp2014@gmail.com>
2024-08-03 22:47:56 -0300

    refactor: updated project-list class code

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -8,11 +8,12 @@ import {
 	ViewChild,
 } from '@angular/core';
 import { projects } from '../../mocks/projects.mockup';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
 	selector: 'app-project-list',
 	standalone: true,
-	imports: [],
+	imports: [ButtonComponent],
 	templateUrl: './project-list.component.html',
 	styleUrl: './project-list.component.scss',
 })
@@ -22,10 +23,12 @@ export class ProjectListComponent {
 	@ViewChild('Table') tableRef!: ElementRef<HTMLElement>;
 
 	currentBannerIndex: number = 0;
-	projectList = projects.sort((a, b) => {
-		// ordernar por ano (decrescente)
-		return b.createdDate.getFullYear() - a.createdDate.getFullYear();
-	});
+	projectList = projects
+		.filter((p) => p.highlight === true)
+		.sort((a, b) => {
+			// ordernar por ano (decrescente)
+			return b.createdDate.getFullYear() - a.createdDate.getFullYear();
+		});
 
 	constructor(
 		@Inject(PLATFORM_ID)
@@ -38,64 +41,65 @@ export class ProjectListComponent {
 
 		const table = this.tableRef.nativeElement;
 
-		this.renderer.listen(table, 'mouseenter', this.onMouseEnter);
-		this.renderer.listen(table, 'mousemove', this.onMouseMove);
-		this.renderer.listen(table, 'mouseover', this.onMouseOver);
-		this.renderer.listen(table, 'mouseleave', this.onMouseLeave);
+		const setBannerInMouseCenter = (event: MouseEvent) => {
+			const bannerContainer = this.bannerContainerRef.nativeElement;
+			const [posX, posY] = [event.clientX, event.clientY];
+
+			bannerContainer.style.left = `${posX}px`;
+			bannerContainer.style.top = `${posY}px`;
+
+			bannerContainer.classList.remove('project_banner_show');
+			bannerContainer.classList.add('project_banner_show');
+		};
+
+		const showBanner = () => {
+			const bannerContainer = this.bannerContainerRef.nativeElement;
+			bannerContainer.classList.remove('project_banner_show');
+			bannerContainer.classList.add('project_banner_show');
+		};
+
+		const hideBanner = () => {
+			const bannerContainer = this.bannerContainerRef.nativeElement;
+			bannerContainer.classList.remove('project_banner_show');
+		};
+
+		const onMouseEnter = (event: MouseEvent) => {
+			setBannerInMouseCenter(event);
+		};
+
+		const onMouseMove = (event: MouseEvent) => {
+			setBannerInMouseCenter(event);
+		};
+
+		const onMouseOver = (event: MouseEvent) => {
+			const bannerContainer = this.bannerContainerRef.nativeElement;
+			const table = this.tableRef.nativeElement;
+			const tbody = table.children[0];
+
+			const target = (event.target as HTMLElement).offsetParent as HTMLElement;
+
+			const nodes = Array.prototype.slice.call(tbody.children);
+			const bannerIndex = nodes.indexOf(target);
+
+			if (bannerIndex === -1) return;
+			if (this.currentBannerIndex === bannerIndex) return;
+			this.currentBannerIndex = bannerIndex;
+
+			const banner = bannerContainer.children[bannerIndex] as HTMLElement;
+			showBanner();
+
+			bannerContainer.scrollTo({
+				top: banner.offsetTop,
+			});
+		};
+
+		const onMouseLeave = () => {
+			hideBanner();
+		};
+
+		this.renderer.listen(table, 'mouseenter', onMouseEnter);
+		this.renderer.listen(table, 'mousemove', onMouseMove);
+		this.renderer.listen(table, 'mouseover', onMouseOver);
+		this.renderer.listen(table, 'mouseleave', onMouseLeave);
 	}
-
-	setBannerInMouseCenter = (event: MouseEvent) => {
-		const bannerContainer = this.bannerContainerRef.nativeElement;
-		const [posX, posY] = [event.clientX, event.clientY];
-
-		bannerContainer.style.left = `${posX}px`;
-		bannerContainer.style.top = `${posY}px`;
-
-		bannerContainer.classList.remove('project_banner_show');
-		bannerContainer.classList.add('project_banner_show');
-	};
-
-	showBanner = () => {
-		const bannerContainer = this.bannerContainerRef.nativeElement;
-		bannerContainer.classList.remove('project_banner_show');
-		bannerContainer.classList.add('project_banner_show');
-	};
-
-	hideBanner = () => {
-		const bannerContainer = this.bannerContainerRef.nativeElement;
-		bannerContainer.classList.remove('project_banner_show');
-	};
-
-	onMouseEnter = (event: MouseEvent) => {
-		this.setBannerInMouseCenter(event);
-	};
-
-	onMouseMove = (event: MouseEvent) => {
-		this.setBannerInMouseCenter(event);
-	};
-
-	onMouseOver = (event: MouseEvent) => {
-		const bannerContainer = this.bannerContainerRef.nativeElement;
-		const table = this.tableRef.nativeElement;
-		const tbody = table.children[0];
-
-		const target = (event.target as HTMLElement).offsetParent as HTMLElement;
-
-		const bannerIndex = Array.prototype.indexOf.call(tbody.children, target);
-
-		if (this.currentBannerIndex === bannerIndex) return;
-		this.currentBannerIndex = bannerIndex;
-
-		const banner = bannerContainer.children[bannerIndex] as HTMLElement;
-
-		this.showBanner();
-
-		bannerContainer.scrollTo({
-			top: banner.offsetTop,
-		});
-	};
-
-	onMouseLeave = () => {
-		this.hideBanner();
-	};
 }
